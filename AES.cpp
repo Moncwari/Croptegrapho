95a1e68e23bd6b425cd6cb8733f95225bda7c1ec
Moncwari <easidorov@edu.hse.ru>
2025-02-17 23:23:25 +0300

    First AES commit

---
+++ AES.cpp
@@ -0,0 +1,19 @@
+#include "AES.h"
+
+
+int main() {
+    vector<uint8_t> key = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x7f, 0x67, 0x98, 0x98, 0x98, 0x98 };
+    SetKeySize(128);
+    vector<uint32_t> roundKeys(Nb * (Nr + 1));
+    KeyExpansion(key, roundKeys);
+    
+    array<array<uint8_t, 4>, 4> state = {{{0x32, 0x43, 0xf6, 0xa8},
+                                           {0x88, 0x5a, 0x30, 0x8d},
+                                           {0x31, 0x31, 0x98, 0xa2},
+                                           {0xe0, 0x37, 0x07, 0x34}}};
+    
+    EncryptBlock(state, roundKeys);
+    
+    cout << "AES Encryption Completed" << endl;
+    return 0;
+}
